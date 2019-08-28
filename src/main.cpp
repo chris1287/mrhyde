0fd58c4fa236cef01ef455290a8c65a28ed1c279
cristiano prato <cristiano.prato@egluetechnologies.com>
2019-08-28 11:25:33 +0200

    add libsodium

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <glog/logging.h>
+#include <sodium.h>
 #include "config.h"
 
 int main(int argc, char *argv[]) {
+    google::InitGoogleLogging(argv[0]);
+    FLAGS_logtostderr = 1;
+
     LOG(INFO) << "start " << FULL_APPLICATION_NAME;
+
+    if(sodium_init() < 0) {
+        LOG(ERROR) << "cannot initialize libsodium";
+        return EXIT_FAILURE;
+    }
+
     LOG(INFO) << "stop " << FULL_APPLICATION_NAME;
-    return 0;
+    return EXIT_SUCCESS;
 }
